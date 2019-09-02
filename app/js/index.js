08231c37358d1d91622cb1f191e779445e86a65d
dabudidabuday <94mir.ms@gmail.com>
2019-09-02 23:48:43 +0300

    first js functionality

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log ('he11o wor1d');
+
+let feedbackButton = document.querySelector (".button__write-us");
+let overlay = document.querySelector (".overlay");
+let modal = document.querySelector (".modal");
+let modalCloseButton = document.querySelector (".modal-close");
+
+feedbackButton.addEventListener ("click", function () {
+    overlay.style.display="block";
+    modal.style.display="block";
+});
+
+modalCloseButton.addEventListener ("click", function () {
+    overlay.style.display="none";
+    modal.style.display="none";
+})
